8268c770d425a526d57cd501f38b43a96d6bd7c0
IsaacMCorpas <7isaac20@gmail.com>
2018-07-23 16:19:53 +0200

    commit de properties cambiado

--- src/test/java/com/nmc/db/TestClienteDAO.java
+++ src/test/java/com/nmc/db/TestClienteDAO.java
@@ -1,8 +1,17 @@
 package com.nmc.db;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ncm.db.ClienteDAO;
+import com.ncm.modelos.Cliente;
 
 public class TestClienteDAO extends TestNCM {
 	private static Logger logger = Logger.getLogger("TestClienteDAO");
@@ -12,6 +21,26 @@ public class TestClienteDAO extends TestNCM {
 		setUpContext();
 	}
 	
-	
+//	@Test
+//	public void testListaCountries() {
+//
+//		logger.log(Level.INFO, "Entrando al test...");
+//
+//		try {
+//			ClienteDAO cliDAO = ClienteDAOFactory.getDAO("mysql");
+//			List<Cliente> listaCountry= cliDAO.getLista();
+//			
+//			
+//			assertTrue(listaCountry != null && !listaCountry.isEmpty());
+//
+//		} catch (Exception ex) {
+//			fail();
+//			ex.printStackTrace();
+//			System.out.println("Error en DAO:" + ex);
+//		}
+//
+//		logger.log(Level.INFO, "Fin de test...");
+//	}
+//	
 	
 }
